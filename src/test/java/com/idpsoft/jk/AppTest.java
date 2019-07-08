2c4e4277b3945cdf6367f8c9230795a9ca32fc5f
chenyouhua <cyhs03@qq.com>
2019-07-08 16:35:51 +0800

    1w1w

---
+++ src/test/java/com/idpsoft/jk/AppTest.java
@@ -0,0 +1,19 @@
+package com.idpsoft.jk;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+	@Test
+	public void testMethodOne() {
+		Assert.assertTrue(true);
+	}
+
+	@Test
+    public void testMethodTwo(){
+        Assert.assertTrue(false);
+    }
+}
